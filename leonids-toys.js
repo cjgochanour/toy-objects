61002de773ebe8a6755a5d7c25c37e1d09c7b0c7
cjgochanour <cjgochanour@gmail.com>
2021-11-16 11:46:12 -0600

    Chapter 4 complete

---
+++ leonids-toys.js
@@ -0,0 +1,25 @@
+const toys = [
+    {
+  id: 1,
+  name: "Tickle Me Elmo",
+  touch: "fluffy",
+  price: 80,
+  isElectronic: true,
+},
+{
+  id: 2,
+  name: "Nerf Gun",
+  touch: "hard plastic",
+  price: 50,
+  isElectronic: false,
+},
+{
+  id: 3,
+  name: "Slinky",
+  touch: "smooth metal",
+  price: 3.5,
+  isElectronic: false,
+},
+]
+
+console.log(toys)
