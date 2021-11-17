1953e9a58071b7ee3e75b69db0379cf538b3761c
cjgochanour <cjgochanour@gmail.com>
2021-11-17 15:09:59 -0600

    added new function to push new object to toys array as well as create a new unique id. ch10 complete

--- leonids-toys.js
+++ leonids-toys.js
@@ -29,7 +29,7 @@ const legoSet = {
   id: 4,
   name: "Millenium Falcoln Lego Set",
   touch: "hard plastic",
-  price: "600",
+  price: 600,
   isElectronic: false,
   isChokingHazard: true,
 }
@@ -38,7 +38,7 @@ const armyMen = {
   id: 5,
   name: "Green Army Men",
   touch: "hard plastic",
-  price: "1",
+  price: 1,
   isElectronic: false,
   isChokingHazard: true,
 }
@@ -46,10 +46,38 @@ const armyMen = {
 toys.push(legoSet)
 toys.push(armyMen)
 
-const toyToFind = 2
+const yoyo = {
+  name: "Yo-Yo",
+  touch: "hard plastic",
+  price: 50,
+  isElectronic: false,
+  isChokingHazard: true
+}
+
+function addToyToStore(newToy){
+  const lastIndex = toys.length - 1
+  const lastToy = toys[lastIndex]
+  const maxID = lastToy.id
+  const newID = maxID + 1
+
+  newToy.id = newID
+  toys.push(newToy)
+}
+
+addToyToStore(yoyo)
 
 for (toy of toys){
-  if (toy.id === toyToFind){
-  toy.price *= 1.05
-  console.log(`The ${toy.name} costs $${toy.price}`)}
+  console.log(`The ${toy.name} costs $${toy.price}.`)
 }
+
+console.log(toys)
+
+//as unprofessional as it may be, the chapter specified to keep the following lines in the code but commented out
+
+// const toyToFind = 2
+
+// for (toy of toys){
+//   if (toy.id === toyToFind){
+//   toy.price *= 1.05
+//   console.log(`The ${toy.name} costs $${toy.price}`)}
+// }
