5e90bab0321e7972bff6eee6fb46c74321b0c161
cjgochanour <cjgochanour@gmail.com>
2021-11-19 14:43:40 -0600

    Sold Perry Scope, Explorer ch2 complete

--- leonids-toys.js
+++ leonids-toys.js
@@ -84,6 +84,32 @@ const removeProduct = (toyID) => {
 
 console.log(removeProduct(3));
 
+const currentInventory = [
+    "Muscle Man",
+    "Dora the Scientist",
+    "Princess Sparkles",
+    "Perry Scope",
+    "Explorer Backpack",
+    "Space Scout Suit",
+    "Geology Mineral Dig Kit",
+    "Perry Scope",
+];
+
+const sellPerryScope = () => {
+    let availableForPurchase = false;
+    for (const toy of currentInventory) {
+        if (toy === "Perry Scope") {
+            availableForPurchase = true;
+        }
+    }
+
+    if (availableForPurchase) {
+        console.log("Enjoy your new Perry Scope");
+    }
+};
+
+sellPerryScope();
+
 // the chapter specified to comment out the "find" code
 
 // const toyToFind = 2
